41f8f292672989a290c529b64f11399c2775e852
Travis Gandy <travis_gandy@yahoo.com>
2020-06-19 07:09:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,5 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 

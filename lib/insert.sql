31a17f24e3bf668773314babf535c5ce53673018
Travis Gandy <travis_gandy@yahoo.com>
2020-06-19 07:09:20 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
